0fcde3b65f4559ace132ca6f72df4ec2cb65b2ff
Guilherme <76690766+guilherme3987@users.noreply.github.com>
2024-06-06 21:08:22 -0300

    Add files via upload
    Atualização do código para criação do módulo do analisador sintático

--- Anasint.c
+++ Anasint.c
@@ -1,66 +1,20 @@
-/*
-Gramática DoBlock
-
-*/
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include "Analex.h"
 #include "Anasint.h"
 
-void prog()
-{
-}
-
-void decl_list_var()
-{
-}
-
-void decl_block_prot()
-{
-}
-
-void block_main()
-{
-}
-
-void decl_var()
-{
-}
-
-void tipo()
-{
-}
-
-void block_def()
-{
-}
-
-void cmd()
-{
-}
-
-void atrib()
-{
-}
-
-void expr()
-{
-}
-
-void expr_simp()
-{
-}
-
-void termo()
-{
-}
-
-void fator()
-{
-}
-
-void op_rel()
-{
-}
+void prog() {}
+void decl_list_var() {}
+void decl_block_prot() {}
+void block_main() {}
+void decl_var() {}
+void tipo() {}
+void block_def() {}
+void cmd() {}
+void atrib() {}
+void expr() {}
+void expr_simp() {}
+void termo() {}
+void fator() {}
+void op_rel() {}
